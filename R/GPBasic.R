b36aeab12b18e78b603065c62b2c231ac61d2184
Trent Henderson <trent.henderson1@outlook.com>
2023-06-15 13:53:08 +1000

    skeleton

---
+++ R/GPBasic.R
@@ -0,0 +1,7 @@
+#' @useDynLib GPBasic
+#' @name GPBasic
+#' @title Fit flexible Gaussian process models in a simple and interpretable way
+#' @description Fit flexible Gaussian process models in a simple and interpretable way
+#' @importFrom Rcpp sourceCpp
+#' @importFrom MASS mvrnorm
+NULL
